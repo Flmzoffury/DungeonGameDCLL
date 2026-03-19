ba0525a588e5d6c9cba6f2fa0ef557ec5c2d8afc
samsi <samsippius@gmail.com>
2026-03-19 18:14:34 -0400

    Work on java doc again

--- src/Potion.java
+++ src/Potion.java
@@ -1,12 +1,27 @@
+/**
+ * Subclass of Treasure, that is able to heal a player by an amount stored within
+ */
 public class Potion extends Treasure
 {
+    /**
+     * Field representing the amount of health given by the potion
+     */
     int potionHealth;
 
+    /**
+     * Constructor setting up potionHealth
+     * @param inputHealth The value assigned to potionHealth
+     */
     public Potion(int inputHealth)
     {
         potionHealth = inputHealth;
     }
 
+    /**
+     * Heals the input player by the health amount
+     * @param inputPlayer The player to be interacted with
+     * @return Returns null, as this implementation of interact() does not need a return value
+     */
     public Object interact(Player inputPlayer)
     {
         inputPlayer.setHealth(inputPlayer.getHealth() + potionHealth);
--- src/Room.java
+++ src/Room.java
@@ -1,12 +1,26 @@
+/**
+ * A room in the dungeon that stores what is within the dungeon
+ */
 public class Room
 {
+    /**
+     * The object that is "in" the room
+     */
     private Object obj;
 
+    /**
+     * Constructor assigns the object within the room
+     * @param inputObject The object that is stored within the room
+     */
     public Room(Object inputObject)
     {
         obj = inputObject;
     }
 
+    /**
+     * Standard toString implementation
+     * @return Returns the name of the object stored in the room, alongside standard formatting.
+     */
     public String toString()
     {
         if (obj == null)
@@ -19,11 +33,19 @@ public class Room
         }
     }
 
+    /**
+     * Assigns the object within the room
+     * @param inputObject The object to be placed within the room
+     */
     public void setObj(Object inputObject)
     {
         obj = inputObject;
     }
 
+    /**
+     * Gets the object inside the room
+     * @return The object inside the room
+     */
     public Object getObj()
     {
         return obj;
