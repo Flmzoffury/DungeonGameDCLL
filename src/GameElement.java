6714327fb3b940850f85a281f730384dc595b079
samsi <samsippius@gmail.com>
2026-03-24 20:43:06 -0400

    Work on javadoc

--- src/GameElement.java
+++ src/GameElement.java
@@ -1,4 +1,11 @@
+/**
+ * Interface defines objects that should appear in a dungeon
+ */
 public interface GameElement
 {
+    /**
+     * toString for elements of the dungeon
+     * @return How the Object should be referred to by the dungeon
+     */
     public String toString();
 }
